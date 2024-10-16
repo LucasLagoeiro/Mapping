d1d79ac44cf56b1c77ff31a0afae6a8ae4a9e718
Lucas Lagoeiro <llagoeiro@outlook.com.br>
2024-10-16 18:23:29 -0300

    Update: Remove some folders

--- ros2_ws/build/pacote_de_exemplos/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/llagoeiro/Desktop/FEI/8_semestre/RoboticaProbFolder/proj2/ros2_ws/install/pacote_de_exemplos'
